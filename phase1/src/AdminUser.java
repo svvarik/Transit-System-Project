5308a9171870eb3014a4eb10eb0233e73a32dea8
Aleashea Ruth Valentine <aleashea.valentine@mail.utoronto.ca>
2018-06-21 15:22:01 -0400

    Started user class

--- phase1/src/AdminUser.java
+++ phase1/src/AdminUser.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
+
 public class AdminUser {
+
+    private String name;
+
+    private String email;
+
+    private ArrayList cards;
+
+    public AdminUser(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.cards = new ArrayList();
+    }
 }
+
