6106396c3b3c88b8434304e116c7d0dc6975bfe3
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-06-21 15:10:14 -0400

    Description of Card class

--- phase1/src/Card.java
+++ phase1/src/Card.java
@@ -1,2 +1,9 @@
 public class Card {
+    //the card has an amount
+    //boolean value tapOn
+    //methods for setting and getting the amount
+    //a timer for tapOn
+    //everytime the user wants to tap on, the timer checks to see if the card has been tapOn for an unusual time
+    //if it, it charges a flat rate, then turns the tap on again with the timer reset to zero
+    //otherwise it just behaves regulary.
 }
