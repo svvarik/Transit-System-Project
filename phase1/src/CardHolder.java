f6ef1899a2480e8dc323193d6660d1d4036df014
Yatharth Khattar <yatharth.khattar@mail.utoronto.ca>
2018-06-22 16:27:50 -0400

    Implemented CardHolder Class

--- phase1/src/CardHolder.java
+++ phase1/src/CardHolder.java
@@ -1,2 +1,55 @@
+import java.util.ArrayList;
+
 public class CardHolder {
+    private String name;
+    private String email;
+    private ArrayList<Card> cards;
+    private int totalCost;
+
+    public CardHolder(String name, String email){
+        this.name = name;
+        this.email = email;
+        this.cards = new ArrayList <Card>();
+        this.totalCost = 0;
+    }
+
+    public void addCard(Card card){
+        this.cards.add(card);
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void removeCard(int cardID){
+        for (int i = 0; i < this.cards.size(); i++){
+            if(cardID == this.cards.get(i).getId()){
+                this.cards.remove(i);
+            }
+        }
+    }
+
+    public void addCard(){
+        this.cards.add(new Card());
+    }
+
+    public void addTotalCost(int fare){
+        this.totalCost += fare;
+    }
+
+    public void modifyCardBalance(int cardID, int value){
+        for (int i = 0; i < this.cards.size(); i++){
+            if(cardID == this.cards.get(i).getId()){
+                this.cards.get(i).addValue(value);
+            }
+        }
+    }
 }
