f7267491c30d9299a43b1e8509e06d6f5836ef22
Faraz Khoshbakhtian <faraz.khoshbakhtian@mail.utoronto.ca>
2018-06-22 21:04:27 -0400

    Added the basic methods and variables to TransitNetwork

--- phase1/src/TransitNetwork.java
+++ phase1/src/TransitNetwork.java
@@ -1,2 +1,32 @@
+import java.util.Date;
+
 public class TransitNetwork {
+    private double flatFare = 2;
+    private double tripFare = 0.5;
+
+    private long startTimeNum;
+    private String startTimeString;
+    private Date date;
+
+    public TransitNetwork(){
+        this.date = new Date();
+        this.startTimeNum = date.getTime();
+        this.startTimeString date.toString();
+    }
+    /** setters for the TransitNetwork's fare values */
+    public void setFlatFare(double fare){ this.flatFare = fare;}
+    public void setTripFare(double fare){this.tripFare = fare;}
+
+
+
+    /** getters for this TransitNetwork's fare values */
+    public double getFlatFare(){ return this.flatFare;}
+    public double getTripFare(){return this.tripFare;}
+
+    /**getter for the TransitNetwork start time in both numericals and String */
+    public long getStartTimeNum(){return this.startTimeNum;}
+    public String getStartTimeString(){return this.startTimeString;}
+
+    /** getRunningTime returns the systems runingtime in ms */
+    public long getRunningTime(){return this.date.getTime() - this.startTimeNum;}
 }
