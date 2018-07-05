bd89b151f5b1962be516b3631bb8eeb7d10c300b
Aleashea Ruth Valentine <aleashea.valentine@mail.utoronto.ca>
2018-07-05 15:03:14 -0400

    made some changes to UserManager

--- phase1/src/UserManager.java
+++ phase1/src/UserManager.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class UserManager {
+
+    public UserManager () {
+
+    }
+
+  public int viewBalance(CardHolder user) {
+    int balance = 0;
+    ArrayList<Card> userCards = user.getCards();
+    for (int i = 0; i < userCards.size(); i++) {
+      balance += ((Card) userCards.get(i)).getBalance();
+    }
+    return balance;
+  }
 }
