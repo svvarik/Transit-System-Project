963de945de4507fb066e893c53fcf11d811a50f2
Yatharth Khattar <yatharth.khattar@mail.utoronto.ca>
2018-07-05 16:46:55 -0400

    Updated TransitNetwork.calcFare

--- phase1/src/TransitNetwork.java
+++ phase1/src/TransitNetwork.java
@@ -1,104 +1,111 @@
 import java.util.Date;
 
 public class TransitNetwork {
-    private double flatFare = 2;
-    private double tripFare = 0.5;
-    private double capFare = 6;
-    private double timeLimit = 7200000;
+  private double flatFare = 2;
+  private double tripFare = 0.5;
+  private double capFare = 6;
+  private double timeLimit = 7200000;
 
-    private long startTimeNum;
-    private String startTimeString;
-    private Date date;
+  private long startTimeNum;
+  private String startTimeString;
+  private Date date;
 
-    public TransitNetwork(){
-        this.date = new Date();
-        this.startTimeNum = date.getTime();
-        this.startTimeString = date.toString();
-    }
-    /** setters for the TransitNetwork's fare values */
-    public void setFlatFare(double fare){ this.flatFare = fare;}
-    public void setTripFare(double fare){this.tripFare = fare;}
-    public void setCapFare(double fare){this.capFare = fare;}
-    public void setTimeLimit(double limit) {this.timeLimit = limit;}
+  public TransitNetwork() {
+    this.date = new Date();
+    this.startTimeNum = date.getTime();
+    this.startTimeString = date.toString();
+  }
+  /** setters for the TransitNetwork's fare values */
+  public void setFlatFare(double fare) {
+    this.flatFare = fare;
+  }
 
+  public void setTripFare(double fare) {
+    this.tripFare = fare;
+  }
 
+  public void setCapFare(double fare) {
+    this.capFare = fare;
+  }
 
-    /** getters for this TransitNetwork's fare values */
-    public double getFlatFare(){ return this.flatFare;}
-    public double getTripFare(){ return this.tripFare;}
-    public double getCapFare() {return this.capFare;}
-    public double getTimeLimit() {return this.timeLimit;}
+  public void setTimeLimit(double limit) {
+    this.timeLimit = limit;
+  }
 
-    /**getter for the TransitNetwork start time in both numericals and String */
-    public long getStartTimeNum(){ return this.startTimeNum;}
-    public String getStartTimeString(){ return this.startTimeString;}
+  /** getters for this TransitNetwork's fare values */
+  public double getFlatFare() {
+    return this.flatFare;
+  }
 
-    /** getRunningTime returns the systems runingtime in ms */
-    public long getRunningTime(){
-        Date d = new Date();
-        return d.getTime() - this.startTimeNum;
-    }
+  public double getTripFare() {
+    return this.tripFare;
+  }
 
-    public double calcFare(Card card, CardMachine cardmachine){
-        if(card.isWithinTimeLimit()){
-            if (card.getAmountSinceLastEffectiveTap() < getCapFare()){
-                if (cardmachine.isEntrance()) {
-                    if (cardmachine.getStation().isFlatRate()) {
-                        return Math.min(getCapFare(), getCapFare() - card.getAmountSinceLastEffectiveTap());
-                    } else {
-                    }
-                } else {
-                    if (cardmachine.getStation().isFlatRate()) {
+  public double getCapFare() {
+    return this.capFare;
+  }
 
-                    } else {
-                        double rate =
-                                Math.abs(cardmachine.getStation().getX() - card.getLastStation().getX())
-                                        * this.tripFare;
-                        return Math.min(rate, getCapFare() - rate);
-                    }
-                }
-            }else{
-                return 0;
-            }
+  public double getTimeLimit() {
+    return this.timeLimit;
+  }
+
+  /** getter for the TransitNetwork start time in both numericals and String */
+  public long getStartTimeNum() {
+    return this.startTimeNum;
+  }
+
+  public String getStartTimeString() {
+    return this.startTimeString;
+  }
+
+  /** getRunningTime returns the systems runingtime in ms */
+  public long getRunningTime() {
+    Date d = new Date();
+    return d.getTime() - this.startTimeNum;
+  }
+
+  public double calcFare(Card card, CardMachine cardmachine) {
+    if (card.isWithinTimeLimit()) {
+      if (card.getAmountSinceLastEffectiveTap() < getCapFare()) {
+        if (cardmachine.isEntrance()) {
+          if (cardmachine.getStation().isFlatRate()) {
+            return Math.min(getCapFare(), getCapFare() - card.getAmountSinceLastEffectiveTap());
+          } else {
+          }
+        } else {
+          if (cardmachine.getStation().isFlatRate()) {
+
+          } else {
+            double rate =
+                Math.abs(cardmachine.getStation().getX() - card.getLastStation().getX())
+                    * getTripFare();
+            return Math.min(rate, getCapFare() - rate);
+          }
         }
-
-        if(!card.isWithinTimeLimit()){
-            card.setAmountSinceLastEffectiveTap(0);
-            card.setLastEffectiveTap();
-            if (cardmachine.isEntrance()) {
-                if (cardmachine.getStation().isFlatRate()) {
-                    return getCapFare();
-                } else {
-                }
-            } else {
-                if (cardmachine.getStation().isFlatRate()) {
-
-                } else {
-                    double rate =
-                            Math.abs(cardmachine.getStation().getX() - card.getLastStation().getX())
-                                    * this.tripFare;
-                    return rate;
-                }
-            }
-
-        }
-
+      } else {
+        return 0;
+      }
     }
-//        if(station.isFlatRate()) { card.deductFare(this.flatFare);}
-//        else{
-//            if(!card.isTapOn()) {card.setTapOn(true);}
-//            else{
-//                Date d = new Date();
-//                if (d.getTime() - card.getLastTapDate().getTime() > 7200000) { // if it has been more than 2 hours since the past trip
-//                  card.deductFare(this.flatFare);
-//                  card.setTapOn();
-//                }
-//                else{
-//                    double tripfare = Math.min(Math.abs(station.getX() - card.getLastStation().getX()) * this.tripFare, this.capFare);
-//                    card.deductFare(tripFare);
-//                    card.setTapOff();
-//                }
-//            }
-//        }
-//    }
+
+    if (!card.isWithinTimeLimit()) {
+      card.setAmountSinceLastEffectiveTap(0);
+      card.setLastEffectiveTap();
+      if (cardmachine.isEntrance()) {
+        if (cardmachine.getStation().isFlatRate()) {
+          return getFlatFare();
+        } else {
+        }
+      } else {
+        if (cardmachine.getStation().isFlatRate()) {
+
+        } else {
+          double rate =
+              Math.abs(cardmachine.getStation().getX() - card.getLastStation().getX())
+                  * this.tripFare;
+          return rate;
+        }
+      }
+    }
+    return 0;
+  }
 }
