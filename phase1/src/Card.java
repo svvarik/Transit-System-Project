c64b7b4a46f6e7f34d800891db053a9efaa13d5e
Sai Vinay Varikooty <sv.varikooty@mail.utoronto.ca>
2018-07-08 16:10:26 -0400

    Refactored card.java to work with new cardmachine and tap system.

--- phase1/src/Card.java
+++ phase1/src/Card.java
@@ -3,42 +3,21 @@ import java.util.Date;
 
 public class Card {
 
-    private boolean tapOn;
+    private static int idAssigner = 0;
 
-    private static int cardId = 0;
-
+    private int cardId;
     private double balance;
-
-    private double amountSinceLastEffectiveTap;
-
-    private ArrayList<Date> tapDates;
-
-    private Station lastTap;
-
+    private ArrayList<Trip> allTrips = new ArrayList<>();
+    private CardHolder owner;
     private boolean suspended;
 
-    private TransitNetwork transitNetwork;
 
-    private Date lastEffectiveTap = null;
-
-    private CardHolder owner;
-
-    private ArrayList<Trip> allTrips = new ArrayList<>();
-
-    //the card has an amount
-    //boolean value tapOn
-    //methods for setting and getting the amount
-    //a timer for tapOn
-    //everytime the user wants to tap on, the timer checks to see if the card has been tapOn for an unusual time
-    //if it, it charges a flat rate, then turns the tap on again with the timer reset to zero
-    //otherwise it just behaves regulary.
-
-    public Card(TransitNetwork tn) {
-        balance = 19;
-        tapOn = false;
-        cardId++;
-        suspended = false;
-        this.transitNetwork = tn;
+    public Card(CardHolder owner) {
+        this.balance = 19;
+        this.suspended = false;
+        this.owner = owner;
+        this.cardId = idAssigner;
+        Card.idAssigner += 1;
     }
 
     public int getCardID() {
@@ -48,6 +27,35 @@ public class Card {
     public double getBalance() {
         return balance;
     }
+
+    public void suspendCard() {
+        this.suspended = true;
+    }
+
+    public CardHolder getOwner(){
+        return this.owner;
+    }
+
+    public int addValue(double value) {
+        if (this.suspended) {
+            // Return some other exception
+            return 1;
+        } else {
+            if (value == 10 | value == 20 | value == 50) {
+                this.balance += value;
+                return 0;
+            } else {
+                // Return some null exception or error
+                return 1;
+            }
+        }
+    }
+
+    // TODO: Handle exceptions - not enough balance left etc.
+    public void deductValue(double fare){
+        this.balance -= fare;
+    }
+
 
     /**
      * Initializes or ends a trip depending on user location, and
@@ -89,64 +97,5 @@ public class Card {
             System.out.println("Error");
         }
     }
-
-
-    public boolean isWithinTimeLimit(){
-        Date d = new Date();
-        if (this.lastEffectiveTap != null)
-            return (Math.abs(this.lastEffectiveTap.getTime() - d.getTime()) < transitNetwork.getTimeLimit());
-        return false;
-    }
-
-    public void addValue(double value) {
-        if (value == 10 | value == 20 | value == 50) {
-            this.balance += value;
-        }
-        else
-        {
-            System.out.println("Not a valid value. Cardholders can only add $10, $20, or $50 to their card balance.");
-        }
-
-        if (this.balance > 0) {
-            this.suspended = false;
-        }
-
-    }
-
-    public void deductFare(double fare) {
-        this.balance -= fare;
-        this.amountSinceLastEffectiveTap += fare;
-        if (this.balance < 0) {
-            this.suspended = true;
-        }
-    }
-
-
-    public void reportStolen() {
-        this.suspended = true;
-    }
-
-    public void setLastTap(Station lastTap) {
-        this.lastTap = lastTap;
-    }
-
-    public void setLastEffectiveTap(){
-        this.amountSinceLastEffectiveTap = 0;
-        Date d = new Date();
-        this.lastEffectiveTap = d;
-    }
-
-    public void setTapOn() { this.tapOn = true; }
-    public void setTapOff() {this.tapOn = false;}
-    public void setOwner(CardHolder owner) {this.owner = owner;}
-    public void setAmountSinceLastEffectiveTap(double amount){this.amountSinceLastEffectiveTap = amount;}
-
-    public boolean isTapOn() {return this.tapOn;}
-
-    public Station getLastStation(){return this.lastTap;}
-    public Date getLastTapDate(){return this.tapDates.get(this.tapDates.size() - 1);}
-    public Date getLastTapOn(){return this.lastEffectiveTap;}
-    public double getAmountSinceLastEffectiveTap(){return this.amountSinceLastEffectiveTap;}
-    public CardHolder getOwner(){return this.owner;}
 
 }
