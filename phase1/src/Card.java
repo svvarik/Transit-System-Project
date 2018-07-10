bb5ac9c6983c26222626357b278b4d653996b228
Yatharth Khattar <yatharth.khattar@mail.utoronto.ca>
2018-07-10 10:54:32 -0400

    Updated Card to deal with suspended cards

--- phase1/src/Card.java
+++ phase1/src/Card.java
@@ -102,46 +102,47 @@ public class Card {
         this.balance -= fare;
     }
 
-
-    /**
-     * Initializes or ends a trip depending on user location, and
-     * deducts fare if required.
-     *
-     * This method will handle exceptions such as incomplete previous trips or
-     * current trips as specified by incomplete trip handlers.
-     *
-     * @param cm the cardmachine this card is tapped on
-     */
-    // TODO: HANDLE EXCEPTIONS
-    // Exception where last trip has not ended, but we are tapping on a cm.entrance
-    // Exception where first trip never started but we are tapping on a cm.exit
-    // Deal with two hour transfers
-    // TODO: Figure out proper return type
-    // If card is suspended, what should we return?
-    public void tapCard(CardMachine cm) {
+  /**
+   * Initializes or ends a trip depending on user location, and deducts fare if required.
+   *
+   * <p>This method will handle exceptions such as incomplete previous trips or current trips as
+   * specified by incomplete trip handlers.
+   *
+   * @param cm the cardmachine this card is tapped on
+   */
+  // TODO: HANDLE EXCEPTIONS
+  // Exception where last trip has not ended, but we are tapping on a cm.entrance
+  // Exception where first trip never started but we are tapping on a cm.exit
+  // Deal with two hour transfers
+  // TODO: Figure out proper return type
+  // If card is suspended, what should we return?
+  public void tapCard(CardMachine cm) {
+    if (!suspended) {
         // if bus:
-        if (cm.getStation() instanceof BusStation){
-            if(cm.isEntrance()){
-                Trip newTrip = new Trip();
-                this.allTrips.add(newTrip);
-                // Calculate fare
-                // Deduct fare from this card
-            } else { // is exit so we end trip
-                this.allTrips.get(allTrips.size()-1).endTrip();
-            }
-        } else if (cm.getStation() instanceof SubwayStation) {
-            if (cm.isEntrance()) {
-                Trip newTrip = new Trip();
-                this.allTrips.add(newTrip);
-            } else { //is exist so we end trip and calc fare
-                this.allTrips.get(allTrips.size()-1).endTrip();
-                // Calculate fare
-                // Deduct fare from this card
-            }
-        } else {
-            // CardMachine either has not been initialized properly or is an invalid cardmachine
-            System.out.println("Error");
+      if (cm.getStation() instanceof BusStation) {
+        if (cm.isEntrance()) {
+          Trip newTrip = new Trip();
+          this.allTrips.add(newTrip);
+          // Calculate fare
+          // Deduct fare from this card
+        } else { // is exit so we end trip
+          this.allTrips.get(allTrips.size() - 1).endTrip();
         }
+      } else if (cm.getStation() instanceof SubwayStation) {
+        if (cm.isEntrance()) {
+          Trip newTrip = new Trip();
+          this.allTrips.add(newTrip);
+        } else { // is exist so we end trip and calc fare
+          this.allTrips.get(allTrips.size() - 1).endTrip();
+          // Calculate fare
+          // Deduct fare from this card
+        }
+      } else {
+        // CardMachine either has not been initialized properly or is an invalid cardmachine
+        System.out.println("Error");
+      }
+    }else{
+        System.out.println("Card is Suspended");
     }
-
+  }
 }
