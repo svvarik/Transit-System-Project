1860e5f5619257ab09ca25385b123e1895151bba
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-07-11 10:16:20 -0400

    javadoc for TransitSystem

--- phase1/src/Main/TransitSystem.java
+++ phase1/src/Main/TransitSystem.java
@@ -11,6 +11,9 @@ import java.util.Date;
 
 public class TransitSystem {
 
+    /**
+     * a TransitManager Specific to this TransitSystem
+     */
     private TransitManager tm = new TransitManager();
 
     private ArrayList<CardHolder> transitCardHolders;
@@ -19,22 +22,40 @@ public class TransitSystem {
     private Date closed;
     private ArrayList<Trip> allTrips;
 
-
+    /**
+     * Constructs a new TransitSystem
+     */
     public TransitSystem(){
         this.transitCardHolders = new ArrayList<>();
         this.stations = new ArrayList<>();
         this.opened = new Date();
     }
 
+    /**
+     * Constructs a TransitSystem with an ArrayList of the TransitSystem's stations, cardholders
+     * @param stations ArrayList<Station>
+     * @param transitCardHolders ArrayList<CardHolder>
+     */
     public TransitSystem(ArrayList<Station> stations, ArrayList<CardHolder> transitCardHolders){
         this.stations = stations;
         this.transitCardHolders = transitCardHolders;
     }
 
+    /**
+     * this method returns this TransitSystem's TransitManager
+     * @return this TransitSystem's TransitManager
+     */
+
     public TransitManager getTransitManager(){
         return this.tm;
     }
 
+    /**
+     * This parameter creats a new CardHolder and adds it to this TransitSystem's cardholders and returns true if everything goes right
+     * @param name name
+     * @param email
+     * @return
+     */
     public boolean  addCardHolder(String name, String email){
         for (CardHolder ch: this.transitCardHolders){
             if (ch.getEmail().equals(email)){
@@ -46,14 +67,25 @@ public class TransitSystem {
         return true;
     }
 
+    /**
+     * adds a station to this TransitSystem's stations
+     * @param station Station to be added to this objects stations
+     */
     public void addStation(Station station) {
         this.stations.add(station);
     }
 
+    /**
+     * adds a Trip to this TransitSystem's trips
+     * @param t  to be added to this objects trips
+     */
     public void addTrip(Trip t){
         this.allTrips.add(t);
     }
 
+    /**
+     * exits the system
+     */
 
     public void exitSystem(){
         this.closed = new Date();
