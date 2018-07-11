8da93b70b43e59c9747b5da340febc0418e8e6ac
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-07-11 10:22:12 -0400

    added javadoc for BusStation

--- phase1/src/TransitSide/BusStation.java
+++ phase1/src/TransitSide/BusStation.java
@@ -2,10 +2,19 @@ package TransitSide;
 
 public class BusStation extends Station {
 
+    /**
+     * constructs a new instance of BusStation
+     */
     public BusStation(){
         super(true);
     }
 
+    /**
+     * constructs a new BusStation with location and name
+     * @param x x coordinate of this station
+     * @param y y coordinate of this station
+     * @param name name of this station
+     */
     public BusStation(int x, int y, String name) {
         super(x, y, true, name);
     }
