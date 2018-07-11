0d3c3a23758f0f5e609b678d656251e5ec187c93
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-07-11 10:26:20 -0400

    javadoc for CardMachine

--- phase1/src/TransitSide/CardMachine.java
+++ phase1/src/TransitSide/CardMachine.java
@@ -8,28 +8,48 @@ public class CardMachine {
 
     private int id;
 
-
+    /**
+     * constructs a new CardMachine
+     * @param entrance determines if this cardMachine is an entrance or an exit
+     * @param station determines what Stations this CardMachine belongs to
+     * @param id this CardMachine's id
+     */
     public CardMachine(boolean entrance, Station station, int id){
         this.id = id;
         this.entrance = entrance;
         this.station = station;
     }
 
-    @Override
+    /**
+     * returns a String representation of this CardMachine
+     * @return the String representation of this CardMachine
+     */
     public String toString() {
         StringBuilder stringRep = new StringBuilder();
         stringRep.append("ID: " + Integer.toString(this.id) + " @ " +  this.station.toString());
         return stringRep.toString();
     }
 
+    /**
+     * returns the Station which this CardMachine belongs to
+     * @return thisCardMachine's station
+     */
     public Station getStation(){
         return this.station;
     }
 
+    /**
+     * returns this CardMachine's id
+     * @return this CardMachine's id
+     */
     public int getId() {
         return this.id;
     }
 
+    /**
+     * returns true if this CardMachine is an entrance
+     * @return true if this CardMachine is an entrance
+     */
     public boolean isEntrance(){
         return this.entrance;
     }
