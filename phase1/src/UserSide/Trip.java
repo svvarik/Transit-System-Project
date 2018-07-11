3ad32becb8c02e225ad951c03f061411b4d21115
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-07-11 11:26:18 -0400

    javadoc for Trip

--- phase1/src/UserSide/Trip.java
+++ phase1/src/UserSide/Trip.java
@@ -12,9 +12,15 @@ public class Trip implements Comparator<Trip>, Comparable<Trip> {
     private Date starDate = null;
     private Date endDate = null;
 
+    /**
+     * constructs a new Trip
+     */
     public Trip(){}
 
-    @Override
+    /**
+     * returns a String representation of this Trip
+     * @return a String representation of this Trip
+     */
     public String toString() {
         String stringRep;
         stringRep = "Start: " + this.start.toString() + " on " + this.starDate +
@@ -22,11 +28,19 @@ public class Trip implements Comparator<Trip>, Comparable<Trip> {
         return stringRep;
     }
 
+    /**
+     * sets the stating point of a trip
+     * @param cardMachine the CardMachine starting of the a trip
+     */
     public void setStart(CardMachine cardMachine){
         this.start = cardMachine;
         Date d = new Date();
         this.starDate = d;
     }
+    /**
+     * sets the ending point of a trip
+     * @param cardMachine the CardMachine ending of the a trip
+     */
 
     public void setEnd(CardMachine cardMachine){
         this.end = cardMachine;
@@ -34,14 +48,35 @@ public class Trip implements Comparator<Trip>, Comparable<Trip> {
         this.endDate = d;
     }
 
+    /**
+     * ends a trip
+     */
     public void endTrip(){
         // pass
     }
 
+    /**
+     * returns this trip CardMachine exit
+     * @return this trip CardMachine exit
+     */
     public CardMachine getEnd() { return end; }
+
+    /**
+     * returns this trip CardMachine entrance
+     * @return this trip CardMachine entrance
+     */
     public CardMachine getStart() { return start; }
 
+    /**
+     * returns this trip end Date
+     * @return this trip end Date
+     */
     public Date getEndDate() { return endDate; }
+
+    /**
+     * returns this trip Start Date
+     * @return this trip Start Date
+     */
     public Date getStarDate() { return starDate; }
 
     // Overriding the compareTo method
