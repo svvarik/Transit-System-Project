4eea9b41dbbc6ef3ae9de81b380d5fe23297462e
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-07-11 10:56:12 -0400

    javadoc for AdminUser

--- phase1/src/UserSide/AdminUser.java
+++ phase1/src/UserSide/AdminUser.java
@@ -12,28 +12,50 @@ public class AdminUser{
     private int operatingCosts;
     private ArrayList<Trip> allTrips;
 
+    /**
+     * constructs a new instance of AdminUser
+     * @param costs this AdminUser's costs
+     */
     public AdminUser (int costs) {
         operatingCosts = costs;
         ArrayList<Trip> allTrips = new ArrayList<>();
     }
 
+    /**
+     * returns the daily profits
+     * @return returns the daily profits
+     */
     public int dailyProfits () {
         return overallRevenue - operatingCosts;
     }
 
+    /**
+     * adds revenue to this AdminUser
+     * @param fare the amount of revenue
+     */
     public void addRevenue(int fare) {
         overallRevenue += fare;
     }
 
+    /**
+     * resets this AdminUser
+     */
     public void reset() {
         overallRevenue = 0;
         ArrayList<Trip> allTrips = new ArrayList<>();
     }
 
-    public void addTrip(Trip newtrip) {
-        allTrips.add(newtrip);
+    /**
+     * adds a trip to this AdminUser trips
+     * @param t trip to be added to this AdminUser's allTrips
+     */
+    public void addTrip(Trip t) {
+        allTrips.add(t);
     }
 
+    /**
+     * calculates the operating costs for this AdminUser
+     */
     public void calculateOperatingCosts(){}
 
 }
