dfc9519e4e823cf58204278852eb4d85cbd959b9
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-07-11 10:39:48 -0400

    javadoc for SubwayStation

--- phase1/src/TransitSide/SubwayStation.java
+++ phase1/src/TransitSide/SubwayStation.java
@@ -2,10 +2,19 @@ package TransitSide;
 
 public class SubwayStation extends Station {
 
+    /**
+     * creates a new SubwayStation , not flatRated
+     */
     public SubwayStation(){
         super(false);
     }
 
+    /**
+     * creates a new SubwayStation, not flatRated
+     * @param x this SubwayStation's first coordinate
+     * @param y this SubwayStation's second coordinate
+     * @param name this SubwayStation's name
+     */
     public SubwayStation(int x, int y, String name) {
         super(x, y, false, name);
     }
