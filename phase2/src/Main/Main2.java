bca714e5ba69e093e3537d2c822281a06ff0eef1
Yatharth Khattar <yatharth.khattar@mail.utoronto.ca>
2018-07-29 16:10:34 -0400

    Update main2 for test purposes

--- phase2/src/Main/Main2.java
+++ phase2/src/Main/Main2.java
@@ -2,17 +2,18 @@ package Main;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
 public class Main2 extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        ScreenController sc = new ScreenController();
-        sc.stageScreen("Main Screen", primaryStage);
-//        Parent root = FXMLLoader.load(getClass().getResource("/GUI/MainScreen/MainScene.fxml"));
-//        primaryStage.setScene(new Scene(root));
-//        primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/TripHistoryScreen/viewTripHistoryScene.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     public static void main(String args[]){
