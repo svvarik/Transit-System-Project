3fd90d2c9a0798314b6561f3b807e7f733f383fd
Yatharth Khattar <yatharth.khattar@mail.utoronto.ca>
2018-07-31 13:37:12 -0400

    Added User Stats Screen

---
+++ phase2/src/GUI/UserStatsScreen/ViewUserStatsSceneController.java
@@ -0,0 +1,60 @@
+package GUI.UserStatsScreen;
+
+import Data.TransitData;
+import GUI.ControllerHelper;
+import Main.TransitSystem;
+import TransitUsers.CardHolder;
+import TransitUsers.Trip;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.ResourceBundle;
+
+public class ViewUserStatsSceneController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private LineChart<Number, Number> userFareGraph;
+
+    @FXML
+    private NumberAxis xAxis;
+
+    @FXML
+    private NumberAxis yAxis;
+
+    private CardHolder cardHolder;
+
+    private XYChart.Series series;
+
+    public void handleBackButton(ActionEvent e){
+        //go back to previous screen
+    }
+
+    public void initialize() {
+        series = new XYChart.Series();
+        for(int i=0; i<=31; i++){
+            series.getData().add(new XYChart.Data(i, Math.random()*10));
+        }
+        if (cardHolder != null) {
+            for(int i=1; i<=31; i++){
+                series.getData().add(new XYChart.Data<>(i, cardHolder.getDailyFare(i, Calendar.getInstance().get(Calendar.MONTH),
+                        Calendar.getInstance().get(Calendar.YEAR))));
+            }
+        }
+        System.out.println("Hello");
+        userFareGraph.getData().add(series);
+    }
+}
