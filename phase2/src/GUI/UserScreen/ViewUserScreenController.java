fcb53207fb877c5825577ca9c8212c7e723ce52d
Yatharth Khattar <yatharth.khattar@mail.utoronto.ca>
2018-08-02 15:25:07 -0400

    Added a screen and controller to handle the all the user things

---
+++ phase2/src/GUI/UserScreen/ViewUserScreenController.java
@@ -0,0 +1,51 @@
+package GUI.UserScreen;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
+
+public class ViewUserScreenController {
+
+    @FXML
+    Button backButton;
+
+    @FXML
+    Button manageCardsButton;
+
+    @FXML
+    Button giftMoneyButton;
+
+    @FXML
+    Button viewStatsButton;
+
+    @FXML
+    Button changeUserInfoButton;
+
+    @FXML
+    public void handleBackButton(ActionEvent e){
+
+    }
+
+    @FXML
+    public void handleManageCardsButton(ActionEvent e){
+
+    }
+
+    @FXML
+    public void handleGiftMoneyButton(ActionEvent e){
+
+    }
+
+    @FXML
+    public void handleViewStatsButton(ActionEvent e){
+
+    }
+
+    @FXML
+    public void handleChangeUserInfoButton(ActionEvent e){
+
+    }
+
+}
