34d9d615ff2171900d426c574a899a9f931b283f
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-08-02 16:31:57 -0400

    populated the class and the controller

--- phase2/src/GUI/DeleteCardScreen/DeleteCardScreenController.java
+++ phase2/src/GUI/DeleteCardScreen/DeleteCardScreenController.java
@@ -1,4 +1,24 @@
 package GUI.DeleteCardScreen;
 
+import GUI.ControllerHelper;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class DeleteCardScreenController {
+
+    @FXML
+    Button backButton;
+
+    @FXML
+    Button deleteCardButton;
+
+    @FXML
+    TextField cardIDEntry;
+
+    ControllerHelper ch = new ControllerHelper();
+
+    public void handleBackButton(){}
+
+    public void handleDeleteCardButton(){}
 }
