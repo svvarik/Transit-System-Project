e355ec875a68b2d48391aeb5d43d040d88599a47
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-08-02 16:15:40 -0400

    added @FXML to the varibales

--- phase2/src/GUI/AddCardScreen/AddCardScreenController.java
+++ phase2/src/GUI/AddCardScreen/AddCardScreenController.java
@@ -1,4 +1,14 @@
 package GUI.AddCardScreen;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class AddCardScreenController {
+
+    @FXML
+    Button backButton;
+
+    @FXML
+    Label completionLabel;
 }
