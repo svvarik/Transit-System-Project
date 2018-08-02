154ded0abbbc29ea336ab782806a04db1935516c
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-08-02 17:05:11 -0400

    populated SuspendCardScreen

---
+++ phase2/src/GUI/SuspendCardScreen/SuspendCardScreenController.java
@@ -0,0 +1,27 @@
+package GUI.SuspendCardScreen;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SuspendCardScreenController {
+
+    @FXML
+    Button backButton;
+
+    @FXML
+    Button suspendCardButton;
+
+    @FXML
+    Label suspendMessage;
+
+    @FXML
+    TextField cardIDField;
+
+    public void handleBackButton(){}
+
+    public void handleSuspendCardButton(){}
+
+
+}
