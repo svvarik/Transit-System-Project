b299d0f24db10347a82376699c419f042d342c9a
Yatharth Khattar <yatharth.khattar@mail.utoronto.ca>
2018-08-02 18:26:07 -0400

    Created Screen for admin user to change info

--- phase2/src/GUI/AdminChangeInfoScreen/AdminChangeInfoController.java
+++ phase2/src/GUI/AdminChangeInfoScreen/AdminChangeInfoController.java
@@ -1,4 +1,44 @@
 package GUI.AdminChangeInfoScreen;
 
+import GUI.ControllerHelper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class AdminChangeInfoController {
+
+    @FXML
+    Button backButton;
+
+    @FXML
+    Button changePasswordButton;
+
+    @FXML
+    Button changeEmailButton;
+
+    @FXML
+    TextField newEmailTextField;
+
+    @FXML
+    PasswordField newPasswordField;
+
+    @FXML
+    Label outcomeLabel;
+
+    ControllerHelper ch = new ControllerHelper();
+
+    public void handleBackButton(ActionEvent e){
+        ch.switchScreens(e, "/GUI/AdminStatistics/AdminMainScene.fxml");
+    }
+
+    public void handlePasswordButton(ActionEvent e){
+
+    }
+
+    public void handleEmailButton(ActionEvent e){
+
+    }
 }
