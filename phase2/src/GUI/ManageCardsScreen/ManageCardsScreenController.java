58de700d08171a4d981e683c644499b1bdfff818
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-08-02 16:03:00 -0400

    populated the ManageCardsScreen

--- phase2/src/GUI/ManageCardsScreen/ManageCardsScreenController.java
+++ phase2/src/GUI/ManageCardsScreen/ManageCardsScreenController.java
@@ -1,4 +1,28 @@
 package GUI.ManageCardsScreen;
 
+import javafx.scene.control.Button;
+
 public class ManageCardsScreenController {
+
+    Button backButton;
+
+    Button addCardButton;
+
+    Button deleteCardButton;
+
+    Button suspendCardButton;
+
+    Button addValueButton;
+
+
+    public void handleBackButton(){}
+
+    public void handleAddCardButton(){}
+
+    public void handleDeleteCardButton(){}
+
+    public void handleSuspendCardButton(){}
+
+    public void handleAddValueButton(){}
+
 }
