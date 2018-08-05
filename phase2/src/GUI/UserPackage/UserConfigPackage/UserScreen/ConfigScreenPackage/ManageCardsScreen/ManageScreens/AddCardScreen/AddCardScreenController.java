6b884391f5161e2c1cb626d9ec1a5f260315a319
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-08-05 10:57:16 -0400

    AddCard and AddValueToCard now work properly in GUI

--- phase2/src/GUI/UserPackage/UserConfigPackage/UserScreen/ConfigScreenPackage/ManageCardsScreen/ManageScreens/AddCardScreen/AddCardScreenController.java
+++ phase2/src/GUI/UserPackage/UserConfigPackage/UserScreen/ConfigScreenPackage/ManageCardsScreen/ManageScreens/AddCardScreen/AddCardScreenController.java
@@ -1,13 +1,22 @@
 package GUI.UserPackage.UserConfigPackage.UserScreen.ConfigScreenPackage.ManageCardsScreen.ManageScreens.AddCardScreen;
 
+import FareSystem.Card;
 import GUI.HelperClasses.ControllerHelper;
 import GUI.GeneralControllerClass.GeneralControllerScreen;
+import TransitUsers.CardHolder;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class AddCardScreenController extends GeneralControllerScreen{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddCardScreenController extends GeneralControllerScreen implements Initializable{
+
+    @FXML
+    Label userLabel;
 
     @FXML
     Button backButton;
@@ -15,10 +24,27 @@ public class AddCardScreenController extends GeneralControllerScreen{
     @FXML
     Label completionLabel;
 
-    ControllerHelper ch = new ControllerHelper();
+    private CardHolder cardHolder;
 
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
     @FXML
     public void handleBackButton(ActionEvent e){
-        ch.switchScreens(e, "/GUI/UserPackage/UserConfigPackage/UserScreen/ConfigScreenPackage/ManageCardsScreen/ManageCardsScreen.fxml");
+        String dest = "/GUI/UserPackage/UserConfigPackage/UserScreen/ConfigScreenPackage/ManageCardsScreen/ManageCardsScreen.fxml";
+        this.getControllerHelper().openSameWindow(dest,
+                this.getTs(), e, this.cardHolder);
+    }
+
+    public void setUpController(){}
+
+    public void setUpController(Object obj) throws ClassCastException{
+        CardHolder ch = (CardHolder) obj;
+        this.cardHolder = ch;
+        this.userLabel.setText(this.cardHolder.toString());
+        Card tempCard = new Card(this.cardHolder, this.getTs().getTapManager());
+        this.cardHolder.addCard(tempCard);
+        this.completionLabel.setText("Your New CardID is " + tempCard.getCardID());
     }
 }
--- phase2/src/GUI/UserPackage/UserConfigPackage/UserScreen/ConfigScreenPackage/ManageCardsScreen/ManageScreens/AddValueScreen/AddValueScreenController.java
+++ phase2/src/GUI/UserPackage/UserConfigPackage/UserScreen/ConfigScreenPackage/ManageCardsScreen/ManageScreens/AddValueScreen/AddValueScreenController.java
@@ -1,14 +1,20 @@
 package GUI.UserPackage.UserConfigPackage.UserScreen.ConfigScreenPackage.ManageCardsScreen.ManageScreens.AddValueScreen;
 
+import FareSystem.Card;
 import GUI.HelperClasses.ControllerHelper;
 import GUI.GeneralControllerClass.GeneralControllerScreen;
+import TransitUsers.CardHolder;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class AddValueScreenController extends GeneralControllerScreen{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddValueScreenController extends GeneralControllerScreen implements Initializable{
 
     @FXML
     Button backButton;
@@ -20,6 +26,9 @@ public class AddValueScreenController extends GeneralControllerScreen{
     Label cardIDMessageLabel;
 
     @FXML
+    Label userLabel;
+
+    @FXML
     Label amountMessageLabel;
 
     @FXML
@@ -28,15 +37,53 @@ public class AddValueScreenController extends GeneralControllerScreen{
     @FXML
     TextField valueAmountTextField;
 
-    ControllerHelper ch = new ControllerHelper();
 
-    public void handleBackButton(ActionEvent e){
-        ch.switchScreens(e, "/GUI/UserPackage/UserConfigPackage/UserScreen/ConfigScreenPackage/ManageCardsScreen/ManageCardsScreen.fxml");
+    private CardHolder cardHolder;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
     }
 
-    public void handleAddValueButton(){}
+    public void handleBackButton(ActionEvent e){
+        String dest = "/GUI/UserPackage/UserConfigPackage/UserScreen/ConfigScreenPackage/ManageCardsScreen/ManageCardsScreen.fxml";
+        this.getControllerHelper().openSameWindow(dest,
+                this.getTs(), e, this.cardHolder);
+    }
+
+    public void handleAddValueButton(){
+        double amount;
+        int destCard;
+        try{
+            destCard = Integer.parseInt(this.cardIDTextField.getText());
+            try{
+                amount = Double.parseDouble((this.valueAmountTextField.getText()));
+                if(this.cardHolder.getCard(destCard) != null){
+                    this.cardHolder.getCard(destCard).addValue(amount);
+                    this.amountMessageLabel.setText("Your Amount");
+                    this.cardIDMessageLabel.setText("Your Card Id");
+                    this.userLabel.setText(amount + " Was Added to "+ destCard);
+                }
+                else{
+                    this.cardIDMessageLabel.setText("Card Does Not Exist");
+                }
+            }catch(Exception e){
+                this.amountMessageLabel.setText("Invalid Input");
+            }
+        }catch(Exception e){
+            this.cardIDMessageLabel.setText("Invalid Input!");
+        }
+    }
+    public void setUpController(){
+
+    }
+
+    public void setUpController(Object obj) throws ClassCastException{
+        CardHolder ch = (CardHolder) obj;
+        this.cardHolder = ch;
+        this.userLabel.setText(this.cardHolder.toString());
 
 
+    }
 
 
 
