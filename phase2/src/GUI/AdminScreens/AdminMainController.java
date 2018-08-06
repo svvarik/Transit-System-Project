cd431bdd1c8467a2118b07917cda46f03cad23e8
Sai Vinay Varikooty <sv.varikooty@mail.utoronto.ca>
2018-08-06 16:12:00 -0400

    Modified methods to follow standard layout throughout program.

--- phase2/src/GUI/AdminScreens/AdminMainController.java
+++ phase2/src/GUI/AdminScreens/AdminMainController.java
@@ -31,15 +31,15 @@ public class AdminMainController extends GeneralControllerScreen {
         Object eve = event.getSource();
         if (eve.equals(changeAccountInfo)){
             String tapScreen = "/GUI/AdminScreens/AdminChangeInfoScreen/AdminChangeInfoScene.fxml";
-            this.getControllerHelper().openSameWindow(tapScreen, this.getTs(), event, this.adminUser);
+            this.getControllerHelper().openSameWindow(tapScreen, this.getTransitSystem(), event, this.adminUser);
         }
         if (eve.equals(manageUsers)){
             String manageUsers = "GUI/AdminScreens/AdminManageUsersScreen/AdminManageUserScene.fxml";
-            this.getControllerHelper().openSameWindow(manageUsers, this.getTs(), event, this.adminUser);
+            this.getControllerHelper().openSameWindow(manageUsers, this.getTransitSystem(), event, this.adminUser);
         }
         if (eve.equals(viewStatistics)){
             String statsScreen = "/GUI/AdminScreens/AdminStatistics/AdminStatisticsScene.fxml";
-            this.getControllerHelper().openSameWindow(statsScreen, this.getTs(), event, this.adminUser);
+            this.getControllerHelper().openSameWindow(statsScreen, this.getTransitSystem(), event, this.adminUser);
         }
     }
 }
