0e0f447584172c95e3f8a757fc9516c09b1320ac
faraz <faraz.khoshbakhtian@mail.utoronto.ca>
2018-08-07 18:57:29 -0400

    added javadocs to ControllerHelper

--- phase2/src/GUI/HelperClasses/ControllerHelper.java
+++ phase2/src/GUI/HelperClasses/ControllerHelper.java
@@ -11,10 +11,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
+/**
+ * Controllerhelper helps the controllers to chnage among themselves and bring up other controllers/screens
+ */
 public class ControllerHelper {
 
-    // Same Window
+    /**
+     * changes the same window into a new screen.
+     * @param event the event that has occured
+     * @param fxmlResource the path of the fxml file we need to switch to.
+     */
     public void switchScreens(ActionEvent event, String fxmlResource){
         try {
             Parent newParent = FXMLLoader.load((getClass().getResource(fxmlResource)));
@@ -29,7 +35,12 @@ public class ControllerHelper {
         }
     }
 
-    // METHOD TO CHANGE SCREEN PASSING NO OBJECTS OVER EXCEPT MODEL
+    /**
+     * This method changes the screen without any objects beside the transitSystem to it
+     * @param fxmlResource the path of the fxml file we need to switch to.
+     * @param model the transitSystem we want to assign to the destination controller
+     * @return returns the parent screen
+     */
     private Parent changeScreen(String fxmlResource, TransitSystem model){
         try {
             FXMLLoader loader = new FXMLLoader();
@@ -45,7 +56,13 @@ public class ControllerHelper {
         }
     }
 
-    // METHOD TO CHANGE SCREEN PASSING OVER ONE OBJECT (STRING, ETC.)
+    /**
+     * This method changes the screen with possiblity of passing an object to the new screen
+     * @param fxmlResource the path of the fxml file we need to switch to
+     * @param model the transitSystem we want to assign to the destination controller
+     * @param object the object we want to assign to the new controller
+     * @return returns the parent screen
+     */
     private Parent changeScreen(String fxmlResource, TransitSystem model, Object object) {
         try {
             FXMLLoader loader = new FXMLLoader();
@@ -61,36 +78,68 @@ public class ControllerHelper {
         }
     }
 
-    // METHOD TO OPEN NEW SCREEN WITH JUST TransitSystem
+    /**
+     * Opens a new window only with a TransitSystem
+     * @param fxmlResource the path of the fxml file we need to switch to
+     * @param model the transitSystem we want to assign to the destination controller
+     * @param event the event which has occurred
+     */
     public void openSameWindow(String fxmlResource, TransitSystem model, ActionEvent event) {
         Parent root = changeScreen(fxmlResource, model);
         setSameWindow(root, event);
     }
 
-    // METHOD TO OPEN NEW SCREEN WITH TransitSystem AND ONE OTHER OBJECT
+    /**
+     * Opens a new window  with a TransitSystem and an extra object
+     * @param fxmlResource the path of the fxml file we need to switch to
+     * @param model the transitSystem we want to assign to the destination controller
+     * @param event the event which has occurred
+     * @param object the object we want to assign to the new controller
+     */
     public void openSameWindow(String fxmlResource, TransitSystem model, ActionEvent event, Object object) {
         Parent root = changeScreen(fxmlResource, model, object);
         setSameWindow(root, event);
     }
 
+    /**
+     * Opens a new window with a TransitSystem and an extra Object
+     * @param fxmlResource the path of the fxml file we need to switch to
+     * @param model the transitSystem we want to assign to the destination controller
+     * @param title the screen's title
+     * @param object the object we want to assign to the new controller
+     */
     public void openNewWindow(String fxmlResource, TransitSystem model, String title, Object object) {
         Parent root = changeScreen(fxmlResource, model, object);
         setNewWindow(root, title);
     }
 
-    // Custom Size New Window
+    /**
+     * Opens a new with a TransitSystem only
+     * @param fxmlResource the path of the fxml file we need to switch to
+     * @param model the transitSystem we want to assign to the destination controller
+     * @param title the screen's title
+     */
     public void openNewWindow(String fxmlResource, TransitSystem model, String title) {
         Parent root = changeScreen(fxmlResource, model);
         setNewWindow(root, title);
     }
 
+    /**
+     * it sets the destination  when we want to open a new window
+     * @param root the scene to be sat
+     * @param title the title of the new screen
+     */
     private void setNewWindow(Parent root, String title) {
         Stage stage = new Stage();
         stage.setTitle(title);
         stage.setScene(new Scene(root));
         stage.show();
     }
-
+    /**
+     * it sets the destination  when we want to open in the same window
+     * @param root the scene to be sat
+     * @param event the event that has occured
+     */
     private void setSameWindow(Parent root, ActionEvent event) {
         Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
         window.setScene(new Scene(root));
